1703148e3efc45a81c74913d428297a12efce63d
tech39294 <happpyprogrammming@gmail.com>
2023-06-04 07:06:22 +0900

    ユーザーテストコード実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.username }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    username              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6)}
     password_confirmation { password }
   end
 end
