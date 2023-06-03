490c6695c9b81622e201470c6aa3ba51948af5ce
tech39294 <happpyprogrammming@gmail.com>
2023-06-03 23:49:05 +0900

    subtaskのバリデーション修正

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,3 +1,13 @@
 class Subtask < ApplicationRecord
   belongs_to :task
+
+  validate :title_and_deadline_must_be_both_present_or_absent
+
+  private
+
+  def title_and_deadline_must_be_both_present_or_absent
+    if (subtask_title.blank? && subtask_deadline.present?) || (subtask_title.present? && subtask_deadline.blank?)
+      errors.add(:base, "Subtask title and deadline must be both present or absent")
+    end
+  end
 end
