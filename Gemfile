d90c58e2d1e72c53e7443cd2ac410b3411a55ddf
tech39294 <happpyprogrammming@gmail.com>
2023-06-04 00:06:58 +0900

    FactoryBotとFakerの導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ end
 gem 'pry-rails'
 
 gem 'rspec-rails', '~> 4.0.0'
+
+gem 'factory_bot_rails'
+
+gem 'faker'
---
+++ spec/factories/tasks.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :task do
+    task_title { Faker::Lorem.sentence(word_count: 3) }
+    task_deadline { Faker::Date.forward(days: 30) }
+    importance_status_id { Faker::Number.between(from: 1, to: 2) }
+    memo { Faker::Lorem.paragraph }
+    association :user 
+  end
+end
