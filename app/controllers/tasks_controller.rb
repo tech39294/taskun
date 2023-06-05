296afc8a838c7c81a7e05a403fbf87ef7bbfe1ea
tech39294 <happpyprogrammming@gmail.com>
2023-06-05 23:58:42 +0900

    RuboCopによる再修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,23 +5,22 @@ class TasksController < ApplicationController
     urgent_important_tasks = Task.includes(:subtasks)
                                  .where(importance_status_id: 1, subtasks: { subtask_deadline: Date.today..(Date.today + 3.days) })
                                  .order(task_deadline: :asc)
-  
+
     urgent_tasks = Task.includes(:subtasks)
                        .where(importance_status_id: 2, subtasks: { subtask_deadline: Date.today..(Date.today + 3.days) })
                        .order(task_deadline: :asc)
-  
+
     important_tasks = Task.includes(:subtasks)
                           .where(importance_status_id: 1, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
                           .order(task_deadline: :asc)
-  
+
     other_tasks = Task.includes(:subtasks)
                       .where(importance_status_id: 2, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
                       .order(task_deadline: :asc)
-  
+
     @tasks = urgent_important_tasks + urgent_tasks + important_tasks + other_tasks
   end
-  
-  
+
   def new
     @task = Task.new
     @task.subtasks.build
