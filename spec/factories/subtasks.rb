dbe58f27cfa0571802239e40a72a96522aa9b0e6
tech39294 <happpyprogrammming@gmail.com>
2023-06-05 23:04:33 +0900

    単体テストコード修正

--- spec/factories/subtasks.rb
+++ spec/factories/subtasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subtask do
     subtask_title { Faker::Lorem.sentence(word_count: 3) }
-    subtask_deadline { 2.days.from_now.to_date }
+    subtask_deadline { 3.days.from_now.to_date }
     association :task
   end
 end
