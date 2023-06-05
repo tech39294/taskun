10912d6c43d7fde26807cb19132d09fe9ed10edd
tech39294 <happpyprogrammming@gmail.com>
2023-06-05 23:57:21 +0900

    一覧機能を一部修正(tasks_controller)
    緊急度高・重要度高→
    緊急度高・重要度低→
    緊急度低・重要度高→
    緊急度低・重要度低　の順に並び替えた
    テストコード問題なし

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,20 +2,26 @@ class TasksController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new]
 
   def index
-    # 期限が3日以内のサブタスクを持つタスクを取得
-    emergent_tasks = Task.includes(:subtasks).select do |task|
-      task.subtasks.any? { |subtask| subtask.subtask_deadline.present? && subtask.subtask_deadline <= 3.days.from_now }
-    end
-
-    # 重要なタスク（期限が3日以内のサブタスクを持つタスク以外）を取得
-    important_tasks = Task.includes(:subtasks).where.not(id: emergent_tasks.map(&:id)).where(importance_status_id: 1).order(task_deadline: :asc)
-
-    # 上記の条件を満たさないタスク（期限が3日以内のサブタスクを持つものも、重要なものも含まない）を取得
-    other_tasks = Task.includes(:subtasks).where.not(id: emergent_tasks.map(&:id)).where.not(importance_status_id: 1).order(task_deadline: :asc)
-
-    @tasks = emergent_tasks + important_tasks + other_tasks
+    urgent_important_tasks = Task.includes(:subtasks)
+                                 .where(importance_status_id: 1, subtasks: { subtask_deadline: Date.today..(Date.today + 3.days) })
+                                 .order(task_deadline: :asc)
+  
+    urgent_tasks = Task.includes(:subtasks)
+                       .where(importance_status_id: 2, subtasks: { subtask_deadline: Date.today..(Date.today + 3.days) })
+                       .order(task_deadline: :asc)
+  
+    important_tasks = Task.includes(:subtasks)
+                          .where(importance_status_id: 1, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
+                          .order(task_deadline: :asc)
+  
+    other_tasks = Task.includes(:subtasks)
+                      .where(importance_status_id: 2, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
+                      .order(task_deadline: :asc)
+  
+    @tasks = urgent_important_tasks + urgent_tasks + important_tasks + other_tasks
   end
-
+  
+  
   def new
     @task = Task.new
     @task.subtasks.build
