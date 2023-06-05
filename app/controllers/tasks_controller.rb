1c49b48837a79fa443ca2a79596258d6c5161d56
tech39294 <happpyprogrammming@gmail.com>
2023-06-05 15:10:29 +0900

    一覧表示機能の実装
    ログインしないとindexに遷移できないことも実装した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,21 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:index, :new]
 
   def index
+    # 期限が3日以内のサブタスクを持つタスクを取得
+    emergent_tasks = Task.includes(:subtasks).select do |task|
+      task.subtasks.any? { |subtask| subtask.subtask_deadline.present? && subtask.subtask_deadline <= 3.days.from_now }
+    end
+
+    # 重要なタスク（期限が3日以内のサブタスクを持つタスク以外）を取得
+    important_tasks = Task.includes(:subtasks).where.not(id: emergent_tasks.map(&:id)).where(importance_status_id: 1).order(task_deadline: :asc)
+
+    # 上記の条件を満たさないタスク（期限が3日以内のサブタスクを持つものも、重要なものも含まない）を取得
+    other_tasks = Task.includes(:subtasks).where.not(id: emergent_tasks.map(&:id)).where.not(importance_status_id: 1).order(task_deadline: :asc)
+
+    @tasks = emergent_tasks + important_tasks + other_tasks
   end
+
 
   def new
     @task = Task.new
