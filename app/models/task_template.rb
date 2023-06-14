e9f0cf5200359b5e4212608c21f2d241f11c50b4
tech39294 <happpyprogrammming@gmail.com>
2023-06-14 19:00:24 +0900

    task_templateモデル修正

--- app/models/task_template.rb
+++ app/models/task_template.rb
@@ -1,8 +1,9 @@
 class TaskTemplate < ApplicationRecord
-- belongs_to :user
-- has_many :tasks
-- has_many :subtask_templates
+belongs_to :user
+has_many :tasks
+has_many :subtask_templates
+accepts_nested_attributes_for :subtask_templates
 
--validates :task_template_title, presence: true
--validates :task_template_deadline, presence: true
+validates :task_template_title, presence: true
+validates :task_template_days, presence: true
 end
