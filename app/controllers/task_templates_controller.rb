437f0113710df69fd4423c120c6229d5071697da
tech39294 <happpyprogrammming@gmail.com>
2023-06-14 19:01:52 +0900

    task_templatesコントローラー編集

---
+++ app/controllers/task_templates_controller.rb
@@ -0,0 +1,23 @@
+class TaskTemplatesController < ApplicationController
+  def new
+    @task_template = TaskTemplate.new
+    @task_template.subtask_templates.build
+  end
+
+  def create
+    @task_template = TaskTemplate.new(task_template_params)
+    if @task_template.save
+      redirect_to tasks_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_template_params
+    params.require(:task_template).permit(:task_template_title, :task_template_days,
+                                 subtask_templates_attributes: [:id, :subtask_template_title, :subtask_template_days, :_destroy]).merge(user_id: current_user.id)                                
+  end
+
+end
