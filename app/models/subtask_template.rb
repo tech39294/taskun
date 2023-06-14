3b9453e7fb748800ff5fd865b58d41ae92267ac7
tech39294 <happpyprogrammming@gmail.com>
2023-06-14 19:01:12 +0900

    subtask_templateモデル編集

--- app/models/subtask_template.rb
+++ app/models/subtask_template.rb
@@ -1,2 +1,13 @@
 class SubtaskTemplate < ApplicationRecord
+  belongs_to :task_template
+
+  validate :title_and_deadline_must_be_both_present_or_absent
+
+  private
+
+  def title_and_deadline_must_be_both_present_or_absent
+    return unless (subtask_template_title.blank? && subtask_template_days.present?) || (subtask_template_title.present? && subtask_template_days.blank?)
+
+    errors.add(:base, 'Subtask title and deadline must be both present or absent')
+  end
 end
