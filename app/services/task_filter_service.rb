d2ce393fde463c3d6c6a8f3deecf68d72f0f7f3b
tech39294 <happpyprogrammming@gmail.com>
2023-06-20 23:20:13 +0900

    新規タスク投稿のデバッグ
    サブタスクなしのタスク投稿をすると、データは保存されるが、indexページに出力されないのでそのデバッグを行った。

--- app/services/task_filter_service.rb
+++ app/services/task_filter_service.rb
@@ -13,13 +13,23 @@ class TaskFilterService
                        .order(task_deadline: :asc)
 
     important_tasks = Task.includes(:subtasks)
-                          .where(importance_status_id: 1, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
-                          .order(task_deadline: :asc)
+                       .where(importance_status_id: 1, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
+                       .order(task_deadline: :asc)
+    
+    important_tasks_without_subtasks = Task.joins(:subtasks)
+                        .where(importance_status_id: 1)
+                        .where(subtasks: { subtask_deadline: nil })
+                        .order(task_deadline: :asc)
 
     other_tasks = Task.includes(:subtasks)
                       .where(importance_status_id: 2, subtasks: { subtask_deadline: (Date.today + 4.days)..Float::INFINITY })
                       .order(task_deadline: :asc)
 
-    urgent_important_tasks + urgent_tasks + important_tasks + other_tasks
+    other_tasks_without_subtasks = Task.joins(:subtasks)
+                      .where(importance_status_id: 2)
+                      .where(subtasks: { subtask_deadline: nil })
+                      .order(task_deadline: :asc)
+
+    urgent_important_tasks + urgent_tasks + important_tasks +important_tasks_without_subtasks+ other_tasks+other_tasks_without_subtasks
   end
 end
