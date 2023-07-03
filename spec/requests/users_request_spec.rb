f44ecb972ac1ed64b65f6c9ed95c1aacb4947226
tech39294 <happpyprogrammming@gmail.com>
2023-07-03 18:51:43 +0900

    アカウント編集機能のテスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  describe 'PUT /users/:id' do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    context '更新内容に問題ない場合' do
+      it 'usernameを編集できる' do
+        sign_in @user
+        new_username = 'newuser'
+        patch "/users/#{@user.id}", params: { user: { username: new_username } }
+        expect(response).to redirect_to(user_path(@user)) 
+        follow_redirect!  
+        expect(response).to have_http_status(:ok)
+        expect(@user.reload.username).to eq(new_username)
+      end
+
+      it 'emailを編集できる' do
+        sign_in @user
+        new_email = 'newemail@email'
+        patch "/users/#{@user.id}", params: { user: { email: new_email } }
+        expect(response).to redirect_to(user_path(@user))  
+        follow_redirect!  # リダイレクトを追跡
+        expect(response).to have_http_status(:ok)
+        expect(@user.reload.email).to eq(new_email)
+      end
+    end
+  end
+
 end
